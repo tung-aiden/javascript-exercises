3c02c1a4d799aff76315e9e642946402066fe8e7
Aiden Tung <aatung@calpoly.edu>
2023-05-11 12:34:16 -0700

    Implemented Filter

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(num_array, ... vals_to_remove) {
+    const filtered_array = num_array.filter(val => (!vals_to_remove.includes(val)));
+    return filtered_array;
 };
 
 // Do not edit below this line
