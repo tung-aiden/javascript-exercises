ad2789d329b3331c856062cac2a5ccbe4e5a5332
Aiden Tung <aatung@calpoly.edu>
2023-05-11 12:08:44 -0700

    Finished first 3 exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let final_string = "";
+    if (num < 0){
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++){
+        final_string += string;
+    }
+    return final_string;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverse_string = "";
+    let rev_string_array = string.split("");
+    for (let i = rev_string_array.length; i > 0; i--){
+        reverse_string += rev_string_array[i-1];
+    }
+    return reverse_string;
 };
 
 // Do not edit below this line
