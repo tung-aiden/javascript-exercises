cb10499a90edf6b4b053a836c1b80b6640ea0183
Aiden Tung <aatung@calpoly.edu>
2023-05-12 14:08:21 -0700

    Assignments 1 - 6 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (typeof(num1) != "number" || typeof(num2) != "number"){
+        return "ERROR";
+    }
+    const large_num = Math.max(num1, num2);
+    const small_num = Math.min(num1,num2);
+    let count = 0;
+    if (large_num < 0|| small_num < 0){
+        return "ERROR";
+    }
+    for (let i = small_num; i <= large_num; i++){
+        count += i;
+    }
+    return count;
 };
 
 // Do not edit below this line
