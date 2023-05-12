7285976fbc9ca97e48c88718664875e95f133f8e
Aiden Tung <aatung@calpoly.edu>
2023-05-12 15:06:22 -0700

    Last test passing using reduce

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(num_array) {
+	let sum = 0;
+  for (let i = 0; i < num_array.length; i++){
+    sum += num_array[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(num_array) {
+  let sum = num_array[0];
+  for (let i = 1; i < num_array.length; i++){
+    sum *= num_array[i];
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(num1, pow) {
+	return num1 ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num <= 0){
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
